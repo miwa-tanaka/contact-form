6582b458b615d44a16d2cc336eee3006f1a72040
Miwa <miwa.tnk9675@gmail.com>
2024-05-26 11:03:25 +0900

    modify: lint settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    dirs: [
-      "src/app/components/",
-    ],
+    dirs: ["src/components/", "src/pages/"],
   },
 };
 
