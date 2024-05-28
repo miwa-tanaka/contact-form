2d35042241988885dec0b63e1ad21a95fa6c80ac
Miwa <miwa.tnk9675@gmail.com>
2024-05-28 21:56:38 +0900

    modify: locale words changes

--- src/constants/locales/en/index.ts
+++ src/constants/locales/en/index.ts
@@ -6,15 +6,18 @@ const en = {
   PREFERRED_CONTACT_METHOD: "Preferred Contact Method",
   EMAIL: "Email",
   TEL: "Tel",
-  NO_HYPHEN: "No hyphen",
   REQUIRED: "Required",
   SUBMIT: "Submit",
   ERROR_MSG_NAME: "Please enter your Name",
+  ERROR_MSG_NAME_MAX_LENGTH: "Please enter your Name within 50 characters",
   ERROR_MSG_EMAIL: "Please enter your Email Address",
+  ERROR_MSG_EMAIL_FORMAT:
+    "The email address format is incorrect. Please check the following points: (contains spaces, missing characters after the @ symbol)",
   ERROR_MSG_PHONE: "Please enter your Phone Number",
-  ERROR_MSG_MAX_LENGTH: "Please enter within 30 characters",
-  ERROR_MSG_FORMAT: "Please enter in the correct format",
+  ERROR_MSG_PHONE_FORMAT: "Please enter your phone number in digits",
   ERROR_MSG_PICK: "Please choose one",
+  ERROR_MSG: "There is an issue with the information entered",
+  CONFIRMATION_MSG: "Your form has been successfully submitted.",
   BACK_TO_TOP: "Back to Top",
 };
 
--- src/constants/locales/ja/index.ts
+++ src/constants/locales/ja/index.ts
@@ -6,15 +6,18 @@ const ja = {
   PREFERRED_CONTACT_METHOD: "ご希望の連絡手段",
   EMAIL: "メール",
   TEL: "お電話",
-  NO_HYPHEN: "ハイフンなし",
   REQUIRED: "必須",
   SUBMIT: "送信",
   ERROR_MSG_NAME: "お名前をご入力ください",
+  ERROR_MSG_NAME_MAX_LENGTH: "お名前は50字以内でご入力ください",
   ERROR_MSG_EMAIL: "メールアドレスをご入力ください",
+  ERROR_MSG_EMAIL_FORMAT:
+    "メールアドレスの形式が正しくありません。以下に該当しないか確認してください。（空白が含まれている。＠以降の文字を入力していない）",
   ERROR_MSG_PHONE: "お電話番号をご入力ください",
-  ERROR_MSG_MAX_LENGTH: "30字以内でご入力ください",
-  ERROR_MSG_FORMAT: "正しい形でご入力ください",
+  ERROR_MSG_PHONE_FORMAT: "お電話番号は数字でご入力ください",
   ERROR_MSG_PICK: "一つお選びください",
+  ERROR_MSG: "入力された情報に問題があります",
+  CONFIRMATION_MSG: "フォームが正常に送信されました。",
   BACK_TO_TOP: "トップへ戻る",
 };
 
