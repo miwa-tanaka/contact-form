ab28a96e4bec76a682f570bf0eeea350f9a71f64
Miwa <miwa.tnk9675@gmail.com>
2024-05-29 16:40:02 +0900

    modify: path fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,28 +5,28 @@ export default function Document() {
     <Html>
       <Head>
         <meta name="description" content="Contact Form" />
-        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="icon" href="/contact-form/favicons/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
+          href="/contact-form/favicons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicons/favicon-32x32.png"
+          href="/contact-form/favicons/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicons/favicon-16x16.png"
+          href="/contact-form/favicons/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="manifest" href="/contact-form/favicons/site.webmanifest" />
         <link
           rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
+          href="/contact-form/favicons/safari-pinned-tab.svg"
           color="#D2CC40"
         />
         <meta name="msapplication-TileColor" content="#D2CC40" />
